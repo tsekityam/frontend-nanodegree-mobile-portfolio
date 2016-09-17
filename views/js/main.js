259a13054cbf2de17132287b305be703bb945577
Tse Kit Yam <me@kytse.com>
2016-09-17 16:53:24 +0800

    avoid forced synchronous layouts
    The value of scroll top should be constant within the loop, so it can
    be extracted from the loop. Now the value will be read once only.

--- views/js/main.js
+++ views/js/main.js
@@ -501,9 +501,10 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
+  var scrollTop = document.body.scrollTop;
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
